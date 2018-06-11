48823625c6cfe38833935206c2e91116e728580d
Christine Benedict <ccbenedict3@gmail.com>
2018-06-11 15:00:55 -0700

    Copied over from Create Movement component to start

--- src/pages/CreateWorkout.js
+++ src/pages/CreateWorkout.js
@@ -1,93 +1,120 @@
 import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
-import {TextField, Button, Card, CardContent} from '@material-ui/core'
-import {registerUser} from "../api"
+import {TextField, Button, Card, CardContent, Snackbar} from '@material-ui/core'
+import {addMove} from "../api"
+import withAuth from '../components/withAuth'
 
 
-class CreateWorkout extends Component{
+class CreateMovement extends Component{
   constructor(props){
     super(props)
     this.state = {
-      user: {
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: "",
-        password_confirmation: ""
+      movement: {
+        movement_name: "",
+        movement_description: "",
+        url: "",
+        bodypart: ""
       },
-      registerSuccess: false,
-      loginSuccess:false
+      setSuccess: false,
+      loginSuccess:false,
+      open: false
     }
   }
+
+  snackOpen = state => () => {
+    this.setState({ open: true });
+  };
+
+  snackClose = () => {
+    this.setState({ open: false });
+  }
+
   handleChange(event){
-    let { user } = this.state
-    user[event.target.id] = event.target.value
-    this.setState({user: user})
+    let { movement } = this.state
+    movement[event.target.id] = event.target.value
+    this.setState({movement: movement})
   }
   handleSubmit(event){
     event.preventDefault()
-    registerUser(this.state.user).then(res =>{
-      this.props.history.push('/dashboard')
+    addMove(this.state.movement).then(res =>{
+      this.setState({ open: true,  movement: {
+        movement_name: "",
+        movement_description: "",
+        url: "",
+        bodypart: "" }
+      })
     })
   }
   enterPressed(event) {
     if(event.key === 'Enter') {
-      registerUser(this.state.user).then(res =>{
-        this.props.history.push('/dashboard')
+      addMove(this.state.movement).then(res =>{
+        this.setState({ open: true,  movement: {
+          movement_name: "",
+          movement_description: "",
+          url: "",
+          bodypart: "" }
+        })
       })
     }
   }
   render(){
-    let form = this.state.user
+    let form = this.state.movement
     return(
       <div id="sign-up-page" onKeyPress={this.enterPressed.bind(this)}>
+        <Snackbar
+          anchorOrigin={ {vertical: 'top', horizontal: 'center'} }
+          open={this.state.open}
+          onClose={this.snackClose.bind(this)}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span style={{display: 'flex',
+    alignItems: 'center'}} id="message-id">Movement Created</span>}
+        />
         <Card className="form-card">
-          <CardContent variant="headline" component="h2">Sign up for Fitology!</CardContent>
+          <CardContent variant="headline" component="h2">Create Movement</CardContent>
           <CardContent id="sign-up-form">
             <span className="text-field">
             <TextField
-                label="First Name"
-                style={{width: '200px'}}
-                id="first_name"
-                value={form.first_name}
+                label="Movement Name"
+                style={{width: '300px'}}
+                id="movement_name"
+                fullWidth
+                value={form.movement_name}
                 onChange={this.handleChange.bind(this)}
             />
           </span><br />
           <span className="text-field">
             <TextField
-                label="Last Name"
-                style={{width: '200px'}}
-                id="last_name"
-                value={form.last_name}
+                label="Movement Description"
+                style={{width: '300px'}}
+                id="movement_description"
+                multiline
+                fullWidth
+                rows="4"
+                value={form.movement_description}
+                onChange={this.handleChange.bind(this)}
+                margin="normal"
+            />
+          </span><br />
+          <span className="text-field">
+            <TextField
+                label="Video URL"
+                style={{width: '300px'}}
+                id="url"
+                fullWidth
+                value={form.url}
                 onChange={this.handleChange.bind(this)}
             />
           </span><br />
           <span className="text-field">
             <TextField
-                label="Email"
-                style={{width: '200px'}}
-                id="email"
-                value={form.email}
-                onChange={this.handleChange.bind(this)}
-            />
-          </span><br />
-          <span className="text-field">
-            <TextField
-                type="password"
-                label="Create Password"
-                style={{width: '200px'}}
-                id="password"
-                value={form.password}
-                onChange={this.handleChange.bind(this)}
-            />
-          </span><br />
-          <span className="text-field">
-            <TextField
-                type="password"
-                label="Confirm Password"
-                style={{width: '200px'}}
-                id="password_confirmation"
-                value={form.password_confirmation}
+                type="text"
+                label="Body Part Focused"
+                style={{width: '300px'}}
+                id="bodypart"
+                fullWidth
+                value={form.bodypart}
                 onChange={this.handleChange.bind(this)}
             />
           </span>
@@ -96,11 +123,11 @@ class CreateWorkout extends Component{
           <Button
             variant="raised"
             color="primary"
-            onClick={this.handleSubmit.bind(this)}>Sign Up</Button>
+            onClick={this.handleSubmit.bind(this)}>Create</Button>
         </span>
         </Card>
       </div>
     )
   }
 }
-export default withRouter(CreateWorkout)
+export default withRouter(withAuth(CreateMovement))
